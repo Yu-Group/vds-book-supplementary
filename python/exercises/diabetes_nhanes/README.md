126235e47a7a39139f47855cb573a103a9e6b83f
Rebecca Barter <rlbarter@gmail.com>
2024-02-26 16:30:47 -0700

    Add ex/diabetes folder for python

---
+++ python/exercises/diabetes_nhanes/README.md
@@ -0,0 +1,6 @@
+https://www.cdc.gov/nchs/nhis/nhis_2016_data_release.htm
+
+Downloaded Sample Adult file (CSV data)
+
+survey summary
+ftp://ftp.cdc.gov/pub/Health_Statistics/NCHS/Dataset_Documentation/NHIS/2016/srvydesc.pdf
---
+++ python/exercises/diabetes_nhanes/dslc_documentation/functions/load_diabetes_data.py
@@ -0,0 +1,54 @@
+
+import pandas as pd
+import numpy as np
+
+def load_diabetes_data(path = "../data/samadult.csv"):
+    # load in the original data
+    diabetes_orig = pd.read_csv(path)
+
+    # take just one person from each household
+    diabetes = diabetes_orig.groupby("HHX") \
+      .sample(1, random_state=24648765) \
+      .reset_index() \
+      .copy()
+    # add an id column
+    diabetes["id"] = np.arange(len(diabetes.index))
+    # create the house_family_person_id column by joining together three ID columns
+    diabetes["house_family_person_id"] = diabetes.apply(lambda x: "_".join(x[["HHX", "FMX", "FPX"]].astype(int).astype(str)), 
+                                                        axis=1)
+    # create the diabetes column
+    diabetes["diabetes"] = (diabetes["DIBEV1"] == 1).astype(int)
+    # create coronary heart disease column
+    diabetes["coronary_heart_disease"] = (diabetes["CHDEV"] == 1).astype(int)
+    # create hypertension column
+    diabetes["hypertension"] = (diabetes["HYPEV"] == 1).astype(int)
+    # create heart_condition column
+    diabetes["heart_condition"] = (diabetes["HRTEV"] == 1).astype(int)
+    # create cancer column
+    diabetes["cancer"] = (diabetes["CANEV"] == 1).astype(int)
+    # create family_history_diabetes column
+    diabetes["family_history_diabetes"] = (diabetes["DIBREL"] == 1).astype(int)
+    # rename remaning relevant columns
+    diabetes = diabetes.rename(columns={"AGE_P": "age",
+                                      "SMKEV": "smoker",
+                                      "SEX": "sex",
+                                      "AWEIGHTP": "weight",
+                                      "BMI": "bmi",
+                                      "AHEIGHT": "height"})
+
+    # select just the relevent columns
+    diabetes = diabetes[["house_family_person_id",
+                        "diabetes",
+                        "age",
+                        "smoker",
+                        "sex",
+                        "coronary_heart_disease",
+                        "weight",
+                        "bmi",
+                        "height",
+                        "hypertension",
+                        "heart_condition",
+                        "cancer",
+                        "family_history_diabetes"]]
+    return(diabetes)
+
