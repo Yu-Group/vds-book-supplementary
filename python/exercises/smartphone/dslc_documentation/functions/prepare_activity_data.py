580e6129bfef4393777d3d380cddb5cad3417715
Rebecca Barter <rlbarter@gmail.com>
2024-02-26 16:34:44 -0700

    Add remaining files in ex/smartphone folder for python

---
+++ python/exercises/smartphone/dslc_documentation/functions/prepare_activity_data.py
@@ -0,0 +1,6 @@
+# Cleaning function for the smartphone activity data
+
+# Define a function for  and pre-process the activity data
+def prepare_activity_data(activity_data): 
+    # fill in the details
+    return activity_data
